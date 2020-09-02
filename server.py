af1c194a602eace44c6d6bb79181fc2650200786
chinnatipchamp <chinnatip_champ@hotmail.com>
2020-09-02 16:26:04 +0700

    Update Server.py

--- server.py
+++ server.py
@@ -5,19 +5,9 @@ from flask import Flask,jsonify
 app = Flask(__name__)
 @app.route('/api/v1/info',methods = ['GET'])
 def info():
-   return jsonify(
-       {
-       "status":'sussess',
-       'name':'Chinnatip taweesuk',
-       'data':'1'
-   }
-   )
+    return jsonify( {"status":'sussess','name':'Chinnatip taweesuk','data':'1'} )
 @app.route('/api/v1/info',methods = ['GET'])
 def getdata():
-    return jsonify(
-           {
-        "status":'sussess'
-    }
-    )
+    return jsonify( {"status":'sussess' })
 if __name__ =='__main__':
     app.run(debug=True,host='0.0.0.0',threaded=True,port=5002)
