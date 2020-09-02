43a1649540d8f27aef471db557383ab450ce388c
chinnatipchamp <chinnatip_champ@hotmail.com>
2020-09-02 16:27:43 +0700

    Update Server.py

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/api/v1/info',methods = ['GET'])
 def info():
     return jsonify( {"status":'sussess','name':'Chinnatip taweesuk','data':'1'} )
-@app.route('/api/v1/info',methods = ['GET'])
+@app.route('/api/v1/getdata',methods = ['GET'])
 def getdata():
     return jsonify( {"status":'sussess' })
 if __name__ =='__main__':
